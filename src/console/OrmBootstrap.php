6ac2ad1f4257fa226d27c8b1881e2203e499bdb3
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-11-28 15:21:06 +0100

    add config start project

---
+++ src/console/OrmBootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+$entity_path = [__DIR__ . '/../domain/entities/'];
+$isDevMode=true;
+$dbParams = parse_ini_file(__DIR__ . '/../config/ticket.ini');
+$config = ORMSetup::createAttributeMetadataConfiguration($entity_path, $isDevMode);
+$connection = DriverManager::getConnection($dbParams, $config);
+$entityManager = new EntityManager($connection, $config);
+
