d335d2c0f19687cb1dff74ec34ac8cff8fc48ac0
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-11-28 20:34:27 +0100

    docker fonctionnel

--- src/console/run1.php
+++ src/console/run1.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$entityManager = require_once __DIR__ . '/OrmBootstrap.php';
+
+$produit = $entityManager->getRepository(Produit::class)->find(4);
+
+print $produit->id . "\n";
+print $produit->numero . "\n";
