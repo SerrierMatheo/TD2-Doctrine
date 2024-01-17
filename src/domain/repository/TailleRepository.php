25eb28702e9987f929d7efd43865ebb14f850bf7
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2024-01-17 14:58:41 +0100

    repository

--- src/domain/repository/TailleRepository.php
+++ src/domain/repository/TailleRepository.php
@@ -1,9 +1,35 @@
 <?php
 
 namespace catadoct\catalog\domain\repository;
+use catadoct\catalog\domain\entities\Taille;
 use Doctrine\ORM\EntityRepository;
 
 class TailleRepository extends EntityRepository
 {
+    public function findById(int $id): ?Taille
+    {
+        return $this->find($id);
+    }
 
+    public function findAllTailles(): array
+    {
+        return $this->findAll();
+    }
+
+    public function saveTaille(Taille $Taille): void
+    {
+        $this->_em->persist($Taille);
+        $this->_em->flush();
+    }
+
+    public function updateTaille(Taille $Taille): void
+    {
+        $this->_em->flush();
+    }
+
+    public function deleteTaille(Taille $Taille): void
+    {
+        $this->_em->remove($Taille);
+        $this->_em->flush();
+    }
 }
--- src/domain/repository/TarifRepository.php
+++ src/domain/repository/TarifRepository.php
@@ -2,9 +2,35 @@
 
 namespace catadoct\catalog\domain\repository;
 
+use catadoct\catalog\domain\entities\Tarif;
 use Doctrine\ORM\EntityRepository;
 
 class TarifRepository extends EntityRepository
 {
+    public function findById(int $id): ?Tarif
+    {
+        return $this->find($id);
+    }
 
+    public function findAllTarifs(): array
+    {
+        return $this->findAll();
+    }
+
+    public function saveTarif(Tarif $Tarif): void
+    {
+        $this->_em->persist($Tarif);
+        $this->_em->flush();
+    }
+
+    public function updateTarif(Tarif $Tarif): void
+    {
+        $this->_em->flush();
+    }
+
+    public function deleteTarif(Tarif $Tarif): void
+    {
+        $this->_em->remove($Tarif);
+        $this->_em->flush();
+    }
 }
